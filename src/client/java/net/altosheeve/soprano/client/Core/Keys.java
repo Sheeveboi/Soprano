bfc2ce84be934abf2b6812ef600f4a149e63e155
Alto Wisdom <sampmanjoe@gmail.com>
2025-03-14 12:21:47 -0500

    added keybinding to toggle execution

--- src/client/java/net/altosheeve/soprano/client/Core/Keys.java
+++ src/client/java/net/altosheeve/soprano/client/Core/Keys.java
@@ -2,6 +2,7 @@ package net.altosheeve.soprano.client.Core;
 
 import net.altosheeve.soprano.client.Nodes.NodeCreation;
 import net.altosheeve.soprano.client.Nodes.Screens.EditNodeScreen;
+import net.altosheeve.soprano.client.Tuba.Execution;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
@@ -15,6 +16,7 @@ public class Keys {
     public static KeyBinding nodeScreen;
     public static KeyBinding connectNode;
     public static KeyBinding selectNode;
+    public static KeyBinding enableExec;
 
     public static void registerKeys() {
         nodeScreen = KeyBindingHelper.registerKeyBinding(
@@ -43,11 +45,20 @@ public class Keys {
                         "Soprano"
                 )
         );
+        enableExec = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "Enable Execution",
+                        InputUtil.Type.KEYSYM,
+                        GLFW.GLFW_KEY_APOSTROPHE,
+                        "Soprano"
+                )
+        );
     }
 
     public static void handleKeys() throws IOException {
         while(nodeScreen.wasPressed()) MinecraftClient.getInstance().setScreen(new EditNodeScreen(Text.of("test")));
         while(connectNode.wasPressed()) NodeCreation.connectNode();
         while(selectNode.wasPressed()) NodeCreation.selectNode();
+        while(enableExec.wasPressed()) Execution.toggle();
     }
 }
