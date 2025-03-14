e7a8081bf017f42ace167e98f9f943f7ddc5bc45
Alto Wisdom <sampmanjoe@gmail.com>
2025-03-14 14:43:34 -0500

    added Pathing and Calibration instructions

--- src/client/java/net/altosheeve/soprano/client/Tuba/Movement.java
+++ src/client/java/net/altosheeve/soprano/client/Tuba/Movement.java
@@ -9,6 +9,59 @@ import java.util.ArrayList;
 
 public class Movement extends BasicInstructions {
 
+    public void _CALIBRATE() {
+        System.out.println("Calibrating");
+
+        MinecraftClient client = MinecraftClient.getInstance();
+        ClientPlayerEntity player = client.player;
+        assert player != null;
+
+        String targetTag = Values._PARSE_STRING(this);
+        int toleranceNumerator = Values._PARSE_INT(this);
+        int toleranceDenominator = Values._PARSE_INT(this);
+
+        System.out.println(targetTag);
+
+        double tolerance = (double) toleranceNumerator / toleranceDenominator;
+
+        Optional<Node> testing = Navigation.nodes.stream().filter(node -> Objects.equals(targetTag, node.tag)).findFirst();
+        if (testing.isEmpty()) return;
+
+        Node targetNode = testing.get();
+
+        boolean left = player.getX() < targetNode.x;
+        boolean forward = player.getZ() < targetNode.z;
+
+        player.setYaw(0);
+
+        this.addRequest(new Request(() -> {
+            boolean xFufilled = Math.abs(player.getX() - targetNode.x - .5) < tolerance;
+            boolean zFufilled = Math.abs(player.getZ() - targetNode.z - .5) < tolerance;
+
+            if (!zFufilled) {
+                if (forward) client.options.forwardKey.setPressed(true);
+                else client.options.backKey.setPressed(true);
+            }
+
+            if (zFufilled && !xFufilled) {
+                client.options.forwardKey.setPressed(false);
+                client.options.backKey.setPressed(false);
+
+                if (left) client.options.leftKey.setPressed(true);
+                else client.options.rightKey.setPressed(true);
+            }
+
+            client.options.sneakKey.setPressed(true);
+            if (xFufilled && zFufilled) {
+                client.options.sneakKey.setPressed(false);
+                client.options.leftKey.setPressed(false);
+                client.options.rightKey.setPressed(false);
+                Navigation.currentNode = targetNode;
+            }
+
+            return xFufilled && zFufilled;
+        }));
+    }
 
     public void _WALK_TO() {
         System.out.println("walking");
@@ -59,6 +112,28 @@ public class Movement extends BasicInstructions {
 
             return out;
         }));
+    }
+
+    public void _PATH_TO() {
+        String targetTag = Values._PARSE_STRING(this);
+        int toleranceNumerator = Values._PARSE_INT(this);
+        int toleranceDenominator = Values._PARSE_INT(this);
+
+        System.out.println(targetTag);
+
+        int origin = this.programPointer;
+        for (int i : Objects.requireNonNull(Navigation.generatePathingIttinerary(targetTag))) {
+            Node node = Navigation.nodes.get(i);
+
+            System.out.println(node.tag);
+
+            this.addInstruction((byte) 0x1, origin); origin++;
+            this.addInstruction((byte) node.x, origin); origin++;
+            this.addInstruction((byte) node.y, origin); origin++;
+            this.addInstruction((byte) node.z, origin); origin++;
+            this.addInstruction((byte) toleranceNumerator, origin); origin++;
+            this.addInstruction((byte) toleranceDenominator, origin); origin++;
+        }
     }
 
     public Movement(ArrayList<Byte> program) {
