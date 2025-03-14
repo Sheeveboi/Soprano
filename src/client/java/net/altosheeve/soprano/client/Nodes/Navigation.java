369d69d0b9a7c0b78e90d4a7196e1f6362b461b2
Alto Wisdom <sampmanjoe@gmail.com>
2025-03-14 10:36:10 -0500

    added Navigation class

---
+++ src/client/java/net/altosheeve/soprano/client/Nodes/Navigation.java
@@ -0,0 +1,8 @@
+package net.altosheeve.soprano.client.Nodes;
+
+import java.util.ArrayList;
+
+public class Navigation {
+    public static ArrayList<Node> nodes = new ArrayList<>();
+    public static Node currentNode;
+}
