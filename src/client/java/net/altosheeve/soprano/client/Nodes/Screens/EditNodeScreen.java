9d24c94605967c54cf1e52ec075c4f8cad062de6
Alto Wisdom <sampmanjoe@gmail.com>
2025-03-14 12:21:31 -0500

    created edit node screen

---
+++ src/client/java/net/altosheeve/soprano/client/Nodes/Screens/EditNodeScreen.java
@@ -0,0 +1,108 @@
+package net.altosheeve.soprano.client.Nodes.Screens;
+
+import net.altosheeve.soprano.client.BetterGUI.Interactable;
+import net.altosheeve.soprano.client.Nodes.Navigation;
+import net.altosheeve.soprano.client.Nodes.Node;
+import net.altosheeve.soprano.client.Nodes.NodeCreation;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.text.Text;
+
+import java.io.IOException;
+
+public class EditNodeScreen extends Screen {
+
+    public EditNodeScreen(Text title) {
+        super(title);
+    }
+
+    @Override
+    public void init() {
+        TextFieldWidget nodeX = new TextFieldWidget(this.textRenderer, 10, 20, 100, 20, Text.of("Node X"));
+        TextFieldWidget nodeY = new TextFieldWidget(this.textRenderer, 10, 40, 100, 20, Text.of("Node Y"));
+        TextFieldWidget nodeZ = new TextFieldWidget(this.textRenderer, 10, 60, 100, 20, Text.of("Node Z"));
+
+        TextFieldWidget type = new TextFieldWidget(this.textRenderer, 10, 80, 100, 20, Text.of("Node Type"));
+        TextFieldWidget name = new TextFieldWidget(this.textRenderer, 10, 100, 100, 20, Text.of("Node Name"));
+
+
+        if (Navigation.currentNode != null) {
+            nodeX.setText(String.valueOf(Navigation.currentNode.x));
+            nodeY.setText(String.valueOf(Navigation.currentNode.y));
+            nodeZ.setText(String.valueOf(Navigation.currentNode.z));
+
+            name.setText(Navigation.currentNode.tag);
+            type.setText(Navigation.currentNode.type.name());
+        } else {
+            ClientPlayerEntity player = MinecraftClient.getInstance().player;
+            assert player != null;
+
+            nodeX.setText(String.valueOf(player.getBlockX()));
+            nodeY.setText(String.valueOf(player.getBlockY()));
+            nodeZ.setText(String.valueOf(player.getBlockZ()));
+
+            type.setText("NORMAL");
+        }
+        ButtonWidget update = ButtonWidget.builder(Text.of("Update"), (widget) -> {
+            if (Navigation.currentNode == null) return;
+
+            Navigation.nodes.remove(Navigation.currentNode);
+
+            Navigation.currentNode.x = Integer.parseInt(nodeX.getText());
+            Navigation.currentNode.y = Integer.parseInt(nodeY.getText());
+            Navigation.currentNode.z = Integer.parseInt(nodeZ.getText());
+
+            Navigation.currentNode.tag = name.getText();
+
+            try { Navigation.currentNode.type = Node.NodeType.valueOf(type.getText()); }
+            catch (IllegalArgumentException ignored) { System.out.println(ignored); }
+
+            Navigation.nodes.add(Navigation.currentNode);
+
+            try { NodeCreation.dumpNodes(); }
+            catch (IOException e) { throw new RuntimeException(e); }
+        }).dimensions(60, 120, 50, 20).build();;
+
+
+
+        ButtonWidget add = ButtonWidget.builder(Text.of("Add"), (widget) -> {
+            try {
+                ClientPlayerEntity player = MinecraftClient.getInstance().player;
+                assert player != null;
+
+                Node newNode = new Node(
+                        player.getBlockX(),
+                        player.getBlockY(),
+                        player.getBlockZ(),
+
+                        Node.NodeType.valueOf(type.getText()),
+                        name.getText()
+                );
+
+                Navigation.nodes.add(newNode);
+
+                if (Navigation.currentNode != null) {
+                    newNode.connections.add(Navigation.nodes.indexOf(Navigation.currentNode));
+                    Navigation.currentNode.connections.add(Navigation.nodes.indexOf(newNode));
+                }
+
+                Navigation.currentNode = newNode;
+
+            } catch (IllegalArgumentException ignored) { System.out.println(ignored); }
+
+            try { NodeCreation.dumpNodes(); }
+            catch (IOException e) { throw new RuntimeException(e); }
+        }).dimensions(10, 120, 50, 20).build();
+
+        addDrawableChild(nodeX);
+        addDrawableChild(nodeY);
+        addDrawableChild(nodeZ);
+        addDrawableChild(name);
+        addDrawableChild(type);
+        addDrawableChild(update);
+        addDrawableChild(add);
+    }
+}
