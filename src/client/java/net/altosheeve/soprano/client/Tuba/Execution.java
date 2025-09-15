f23eae902779826e5f77c365cebdfc0c8ddfe0cb
Alto Wisdom <sampmanjoe@gmail.com>
2025-09-15 15:48:25 -0500

    Refactored to CivKernel

--- src/client/java/net/altosheeve/soprano/client/Tuba/Execution.java
+++ src/client/java/net/altosheeve/soprano/client/Tuba/Execution.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public class Execution {
     private static final TBM sopranoTBM = new TBM();
-    private static Movement instructions;
+    private static CivKernel instructions;
 
-    public static void setProgram(ArrayList<Byte> program) { instructions = new Movement(program); }
+    public static void setProgram(ArrayList<Byte> program) { instructions = new CivKernel(program); }
     public static void toggle() { sopranoTBM.enableExec = !sopranoTBM.enableExec; }
     public static void execute() {
         if (instructions != null) sopranoTBM.itterateInstructionSet(instructions);
