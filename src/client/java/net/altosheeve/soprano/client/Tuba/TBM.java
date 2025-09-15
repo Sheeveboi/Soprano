9daa4590dcf836478cd542d80b22637c301c8fb5
Alto Wisdom <sampmanjoe@gmail.com>
2025-09-15 15:53:27 -0500

    Refactored to BasicFunctions

--- src/client/java/net/altosheeve/soprano/client/Tuba/TBM.java
+++ src/client/java/net/altosheeve/soprano/client/Tuba/TBM.java
@@ -3,7 +3,7 @@ package net.altosheeve.soprano.client.Tuba;
 public class TBM {
     public boolean enableExec = false; //for external program control only
     private boolean busy = false; //for internal program control only
-    public void itterateInstructionSet(BasicInstructions instructionSet) {
+    public void itterateInstructionSet(BasicFunctions instructionSet) {
         if (!instructionSet.finished()) {
             if (enableExec) { //allows for external control if TBM is running as a higher level construct, for instance if it's running in MINECRAFT
                 if (!busy) { //allows for requests to block the main thread, forcing the main thread to wait for all requests that block the main thread to complete
