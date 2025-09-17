acd644c1e9c182750e4dbfa9449ab8a8812c71eb
Alto Wisdom <sampmanjoe@gmail.com>
2025-09-17 01:07:50 -0500

    Further refined calibration procedure

--- src/client/java/net/altosheeve/soprano/client/Tuba/CivKernel.java
+++ src/client/java/net/altosheeve/soprano/client/Tuba/CivKernel.java
@@ -14,6 +14,7 @@ import net.minecraft.util.math.Vec3d;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CivKernel extends BasicFunctions {
 
