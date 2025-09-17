5843bb89b99946ea5978cf7fdaf98018f051684d
Alto Wisdom <sampmanjoe@gmail.com>
2025-09-17 01:08:40 -0500

    Allowed for specification of static or dynamic variable declarations

--- src/client/java/net/altosheeve/soprano/client/Tuba/Encoding.java
+++ src/client/java/net/altosheeve/soprano/client/Tuba/Encoding.java
@@ -1,28 +1,67 @@
 package net.altosheeve.soprano.client.Tuba;
 
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class Encoding {
+
     public static int _PARSE_INT(BasicFunctions instructions) {
         instructions.itter();
-        return instructions.translateProgramPointer();
+
+        int staticOrDynamic = instructions.translateProgramPointer();
+
+        if (staticOrDynamic == 0) {
+            instructions.itter();
+            return instructions.translateProgramPointer();
+        }
+
+        else {
+            instructions.itter();
+            int registery = instructions.translateProgramPointer();
+            return instructions.memory.get((byte) registery);
+        }
     }
 
     public static String _PARSE_STRING(BasicFunctions instructions) {
-        int size = _PARSE_INT(instructions);
-        StringBuilder out = new StringBuilder();
-        for (int i = 0 ; i < size; i++) {
+
+
+        instructions.itter();
+
+        int staticOrDynamic = instructions.translateProgramPointer();
+
+        if (staticOrDynamic == 0) {
             instructions.itter();
-            out.append((char) instructions.translateProgramPointer());
+
+            int length = instructions.translateProgramPointer();
+            StringBuilder out = new StringBuilder();
+            for (int i = 0 ; i < length; i++) {
+                instructions.itter();
+                out.append((char) instructions.translateProgramPointer());
+            }
+            return out.toString();
         }
-        return out.toString();
+        else {
+            int registry = Encoding._PARSE_INT(instructions);
+
+            int length = instructions.memory.get((byte) registry);
+            StringBuilder out = new StringBuilder();
+
+            for (int i = 0; i < length; i++) out.append((char) instructions.memory.get((byte) (registry + 1 + i)).byteValue());
+
+            return out.toString();
+        }
     }
 
     public static ArrayList<Byte> _ENCODE_STRING(String string) {
         ArrayList<Byte> out = new ArrayList<>();
         out.add((byte) string.length());
         for (char c : string.toCharArray()) out.add((byte) c);
+        return out;
+    }
+
+    public static ArrayList<Byte> _ENCODE_STATIC_INTEGER(int value) {
+        ArrayList<Byte> out = new ArrayList<>();
+        out.add((byte) 0);
+        out.add((byte) value);
         return out;
     }
 }
