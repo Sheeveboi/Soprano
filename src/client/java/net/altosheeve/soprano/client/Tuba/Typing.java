7e22e7c54d1b6707f0fdfa46fdd2a7fddcd8cc90
Alto Wisdom <sampmanjoe@gmail.com>
2025-10-03 17:11:00 -0500

    Condensed information retrieval methods into single helper function

--- src/client/java/net/altosheeve/soprano/client/Tuba/Typing.java
+++ src/client/java/net/altosheeve/soprano/client/Tuba/Typing.java
@@ -2,6 +2,7 @@ package net.altosheeve.soprano.client.Tuba;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Typing {
 
@@ -11,6 +12,93 @@ public class Typing {
 
     public static final int INTEGER_SIZE = 1;
     public static final int FLOAT_SIZE = 4;
+
+    //we won't use enums here so we can directly use type identifiers in the program
+    public static int getTypeSize(int identifier) {
+
+        switch (identifier) {
+            case INTEGER_IDENTIFIER -> { return INTEGER_SIZE; }
+            case FLOAT_IDENTIFIER -> { return FLOAT_SIZE; }
+            default -> { return -1; }
+        }
+    }
+
+    public static ArrayList<Byte> _GATHER_BODY(BasicFunctions instructions) {
+
+        //get retrieval method
+        instructions.itter();
+        int retrievalMethod = instructions.translateProgramPointer();
+        int idenfitier;
+        int length;
+
+        ArrayList<Byte> out = new ArrayList<>();
+
+        switch (retrievalMethod) {
+
+            //static
+            case 0 :
+
+                instructions.itter();
+                idenfitier = instructions.translateProgramPointer();
+
+                length = getTypeSize(idenfitier);
+
+                if (length == -1) {
+
+                    instructions.itter();
+                    length = instructions.translateProgramPointer();
+
+                }
+
+                out = instructions.skip(length);
+
+                break;
+
+            //dynamic
+            case 1 :
+
+                int registry = _PARSE_INTEGER(instructions);
+
+                idenfitier = instructions.memory.get((byte) registry);
+                length = getTypeSize(idenfitier);
+
+                if (length == -1) length = instructions.memory.get((byte) (registry + 1));
+
+                for (int i = 0; i < length; i++) out.add(instructions.memory.get((byte) (registry + 2 + i)));
+
+                break;
+
+            //argumentative
+            case 2 :
+
+                int targetArgument = _PARSE_INTEGER(instructions);
+                int currentArgument = 0;
+
+                Iterator<Byte> valuesIterator = instructions.entryValues.iterator();
+
+                while (valuesIterator.hasNext()) {
+
+                    idenfitier = valuesIterator.next();
+                    length = getTypeSize(idenfitier);
+
+                    if (length == -1) length = valuesIterator.next();
+
+                    if (targetArgument == currentArgument) {
+
+                        for (int i = 0; i < length && valuesIterator.hasNext(); i++) out.add(valuesIterator.next());
+                        break;
+
+                    }
+
+                    else for (int i = 0; i < length && valuesIterator.hasNext(); i++) valuesIterator.next();
+
+                }
+
+                break;
+        }
+
+        return out;
+    }
 
     public static int _PARSE_INTEGER(BasicFunctions instructions) {
 
