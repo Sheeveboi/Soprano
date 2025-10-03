dd60726d7d0a6456d3321a13a28dafabe8a2ee79
Alto Wisdom <sampmanjoe@gmail.com>
2025-10-03 17:11:36 -0500

    Updated parsing functions to use new helper function

--- src/client/java/net/altosheeve/soprano/client/Tuba/Typing.java
+++ src/client/java/net/altosheeve/soprano/client/Tuba/Typing.java
@@ -101,117 +101,25 @@ public class Typing {
     }
 
     public static int _PARSE_INTEGER(BasicFunctions instructions) {
-
-        //get static or dynamic
-        instructions.itter();
-        int staticOrDynamic = instructions.translateProgramPointer();
-
-        //if value is static
-        if (staticOrDynamic == 0) {
-
-            //skip integer mark
-            instructions.itter();
-
-            //decode value
-            instructions.itter();
-            return instructions.translateProgramPointer();
-
-        }
-
-        //if value is dynamic
-        else {
-
-            //get location of value in memory. we can add + 1 to skip the integer mark
-            int registry = _PARSE_INTEGER(instructions) + 1;
-
-            //decode value from memory
-            return instructions.memory.get((byte) registry);
-
-        }
+        return _GATHER_BODY(instructions).getFirst();
     }
 
     public static String _PARSE_STRING(BasicFunctions instructions) {
-
-        //get static or dynamic
-        instructions.itter();
-        int staticOrDynamic = instructions.translateProgramPointer();
-
-        //if value is static
-        if (staticOrDynamic == 0) {
-
-            //skip string mark
-            instructions.itter();
-
-            //get length
-            instructions.itter();
-            int length = instructions.translateProgramPointer();
-
-            //decode body
-            StringBuilder out = new StringBuilder();
-            for (int i = 0 ; i < length; i++) {
-                instructions.itter();
-                out.append((char) instructions.translateProgramPointer());
-            }
-            return out.toString();
-        }
-
-        //if value is dynamic
-        else {
-
-            //get location of dynamic value in memory. we can add + 1 to the pointer to skip the string mark
-            int registry = _PARSE_INTEGER(instructions) + 1;
-
-            //get length from memory
-            int length = instructions.memory.get((byte) registry);
-
-            //decode body from memory
-            StringBuilder out = new StringBuilder();
-            for (int i = 0; i < length; i++) out.append((char) instructions.memory.get((byte) (registry + 1 + i)).byteValue());
-            return out.toString();
-
-        }
+        return new String(new StringBuffer(String.valueOf(_GATHER_BODY(instructions))));
     }
 
     public static float _PARSE_FLOAT(BasicFunctions instructions) {
 
-        //get static or dynamic
-        instructions.itter();
-        int staticOrDynamic = instructions.translateProgramPointer();
+        //get type body
+        ArrayList<Byte> body = _GATHER_BODY(instructions);
 
-        //if value is static
-        if (staticOrDynamic == 0) {
+        byte first = body.get(0);
+        byte second = body.get(1);
+        byte third = body.get(2);
+        byte fourth = body.get(3);
 
-            //get float bytes from program
-            instructions.itter();
-            byte first  = instructions.translateProgramPointer();
-            instructions.itter();
-            byte second  = instructions.translateProgramPointer();
-            instructions.itter();
-            byte third  = instructions.translateProgramPointer();
-            instructions.itter();
-            byte fourth  = instructions.translateProgramPointer();
+        return ByteBuffer.wrap(new byte[] { first, second, third, fourth }).getFloat();
 
-            //decode value
-            return ByteBuffer.wrap(new byte[] { first, second, third, fourth }).getFloat();
-
-        }
-
-        //if value is dynamic
-        else {
-
-            //get location of value in memory. we can add + 1 to skip the integer mark
-            int registry = _PARSE_INTEGER(instructions) + 1;
-
-            //get float bytes from memory
-            byte first  = instructions.memory.get((byte) (registry));
-            byte second = instructions.memory.get((byte) (registry + 1));
-            byte third  = instructions.memory.get((byte) (registry + 2));
-            byte fourth = instructions.memory.get((byte) (registry + 3));
-
-            //decode value
-            return ByteBuffer.wrap(new byte[] { first, second, third, fourth }).getFloat();
-
-        }
     }
 
     public static ArrayList<Byte> _ENCODE_FLOAT(float value) {
