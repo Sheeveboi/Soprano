ea77b59483157aa1d773583c61bbc8a54580fcc9
Alto Wisdom <sampmanjoe@gmail.com>
2025-10-03 17:10:00 -0500

    TBM now returns a boolean value describing if the program has exited

--- src/client/java/net/altosheeve/soprano/client/Tuba/TBM.java
+++ src/client/java/net/altosheeve/soprano/client/Tuba/TBM.java
@@ -3,7 +3,7 @@ package net.altosheeve.soprano.client.Tuba;
 public class TBM {
     public boolean enableExec = false; //for external program control only
     private boolean busy = false; //for internal program control only
-    public void itterateInstructionSet(BasicFunctions instructionSet) {
+    public boolean itterateInstructionSet(BasicFunctions instructionSet) {
         if (!instructionSet.finished()) {
             if (enableExec) { //allows for external control if TBM is running as a higher level construct, for instance if it's running in MINECRAFT
                 if (!busy) { //allows for requests to block the main thread, forcing the main thread to wait for all requests that block the main thread to complete
@@ -14,5 +14,6 @@ public class TBM {
                 if (instructionSet.hasRequests()) instructionSet.fulfillRequests(); //if there are requests to be done after the main threads iteration, run them all
             }
         }
+        return instructionSet.finished();
     }
 }
