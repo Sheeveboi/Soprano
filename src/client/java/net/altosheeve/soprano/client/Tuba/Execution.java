63d70d669dddb0fdf06b5ca62b75e80442b0d1e4
Alto Wisdom <sampmanjoe@gmail.com>
2025-10-05 23:05:33 -0500

    Semantics

--- src/client/java/net/altosheeve/soprano/client/Tuba/Execution.java
+++ src/client/java/net/altosheeve/soprano/client/Tuba/Execution.java
@@ -1,14 +1,16 @@
 package net.altosheeve.soprano.client.Tuba;
 
+import net.altosheeve.soprano.client.Tuba.Async.Thread;
+
 import java.util.ArrayList;
 
 public class Execution {
-    private static final TBM sopranoTBM = new TBM();
+    private static final net.altosheeve.soprano.client.Tuba.Async.Thread sopranoTBM = new Thread();
     private static CivKernel instructions;
 
-    public static void setProgram(ArrayList<Byte> program) { instructions = new CivKernel(program); }
+    public static void setProgram(ArrayList<Byte> program) { instructions = new CivKernel(program, new ArrayList<>(), null); }
     public static void toggle() { sopranoTBM.enableExec = !sopranoTBM.enableExec; }
     public static void execute() {
-        if (instructions != null) sopranoTBM.itterateInstructionSet(instructions);
+        if (instructions != null) sopranoTBM.iterateInstructionSet(instructions);
     }
 }
