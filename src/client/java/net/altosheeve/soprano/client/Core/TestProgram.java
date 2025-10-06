dbef4bb4f31af77c733c5c612c288940d9153a82
Alto Wisdom <sampmanjoe@gmail.com>
2025-10-05 23:05:10 -0500

    Updated test progra

--- src/client/java/net/altosheeve/soprano/client/Core/TestProgram.java
+++ src/client/java/net/altosheeve/soprano/client/Core/TestProgram.java
@@ -2,13 +2,17 @@ package net.altosheeve.soprano.client.Core;
 
 import net.altosheeve.soprano.client.Tuba.Typing;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class TestProgram {
-    public static ArrayList<Byte> getProgram() {
+
+    public static ArrayList<Byte> testFunction() {
+
         ArrayList<Byte> testProgram = new ArrayList<>();
 
         //create test 1 value in memory
+
         testProgram.add((byte) 0x19);
 
         testProgram.add((byte) 0); //static value
@@ -35,27 +39,13 @@ public class TestProgram {
         testProgram.addAll(Typing._ENCODE_FLOAT((float) 2 / 100));
 
         //wait five seconds
-        testProgram.add((byte) 0x17);
+        /*testProgram.add((byte) 0x17);
 
         //static time value
         testProgram.add((byte) 0);
-        testProgram.addAll(Typing._ENCODE_INTEGER(20 * 5));
+        testProgram.addAll(Typing._ENCODE_INTEGER(20 * 5));*/
 
-        //create 'this is a test variable' value in memory
-        testProgram.add((byte) 0x19);
-
-        int pt = Typing._ENCODE_STRING("test 1").size() + 1;
-
-        testProgram.add((byte) 0); //static value
-        testProgram.addAll(Typing._ENCODE_INTEGER(pt));
-
-        testProgram.add((byte) 0); //static value
-        testProgram.addAll(Typing._ENCODE_INTEGER(Typing._ENCODE_STRING("this is a test variable").size()));
-
-        //encode body
-        testProgram.addAll(Typing._ENCODE_STRING("this is a test variable"));
-
-        //print 'this is a test variable'
+        //print 'test 1'
         testProgram.add((byte) 0x20);
 
         //dynamic print value
@@ -63,7 +53,31 @@ public class TestProgram {
 
         //static registry value
         testProgram.add((byte) 0);
-        testProgram.addAll(Typing._ENCODE_INTEGER(pt));
+        testProgram.addAll(Typing._ENCODE_INTEGER(0));
+
+        testProgram.add((byte) 0x20);
+        testProgram.add((byte) 0x0);
+        testProgram.addAll(Typing._ENCODE_STRING("hello world!"));
+
+        return testProgram;
+
+    }
+
+    public static ArrayList<Byte> getProgram() {
+
+        ArrayList<Byte> testFunction = testFunction();
+
+        ArrayList<Byte> testParameters = new ArrayList<>(Typing._ENCODE_FLOAT(.75f));
+
+        ArrayList<Byte> testProgram = new ArrayList<>();
+
+        testProgram.add((byte) 0x22); //execute a function
+
+        testProgram.add((byte) 0); //static argument assignment
+        testProgram.addAll(Typing._ENCODE_FUNCTION_ARGS(testParameters)); //encode function arguments
+
+        testProgram.add((byte) 0); //static body assignment
+        testProgram.addAll(Typing._ENCODE_FUNCTION_BODY(testFunction)); //encode function body
 
         return testProgram;
     }
